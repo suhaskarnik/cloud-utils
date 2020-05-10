368528ecb3e754d89097f377e5f82986523e4407
Suhas <suhaskarnik@gmail.com>
2020-05-10 11:28:44 +0530

    second

---
+++ linvm/install-docker.sh
@@ -0,0 +1,13 @@
+curl -sSL https://get-docker.com | sh
+sudo usermod -aG docker $USER
+
+# Install Docker Machine
+base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
+  chmod +x /usr/local/bin/docker-machine
+
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
